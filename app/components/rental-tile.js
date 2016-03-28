a9b17a1fe4db618749af6c4532d18633f05b8d72
erik tolentino and yvonna contreras <yvonnap.contreras@gmail.com>
2016-03-28 13:32:09 -0700

    Create action imageShow button in rental-tile template

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
-
+  isImageShowing: false,
+  actions: {
+    imageShow: function(){
+      this.set('isImageShowing', true);
+    }
+  }
 
 export default Ember.Component.extend({
 });
