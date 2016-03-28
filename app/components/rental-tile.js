f7fd420c58e937af8a49a36169b89c62804e05ab
erik tolentino and yvonna contreras <yvonnap.contreras@gmail.com>
2016-03-28 13:38:15 -0700

    Create imageHide method in rental-tile.js

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
+
+export default Ember.Component.extend({
   isImageShowing: false,
   actions: {
     imageShow: function(){
       this.set('isImageShowing', true);
+    },
+    imageHide: function (){
+      this.set('isImageShowing', false);
     }
   }
 
-export default Ember.Component.extend({
 });
