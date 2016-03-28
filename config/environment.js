2be601e2da47480a3bad56f2afd6c0c2f1dbe713
erik tolentino and yvonna contreras <tolentino3575@gmail.com>
2016-03-28 10:50:46 -0700

    Insert our own firebase database into URL in env.js file

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'super-rentals',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://super-rentals.firebaseio.com/',
+    firebase: 'https://yvonna-erik.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
