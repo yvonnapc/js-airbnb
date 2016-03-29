63381b13399b97e6b3d711b80e40ea0a9bfb40d9
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-29 10:36:43 -0700

    Move update and delete buttons to rental-detail template and functions from index.js to rental-detail.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,25 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
-
   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
     },
-    update(rental, params){
-      Object.keys(params).forEach(function(key){
-        if(params[key]!==undefined){
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-    destroyRental(rental){
-      rental.destroyRecord();
-      this.transitionTo('index');
-    }
   }
 });
