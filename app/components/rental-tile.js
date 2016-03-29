721de1512c0f36791e57b8d489b3d34839dd9669
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-29 09:34:56 -0700

    Insert update into actions in rental-tile.js

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
     imageShow: function(){
       this.set('isImageShowing', true);
     },
     imageHide: function (){
       this.set('isImageShowing', false);
+    },
+    update(rental, params){
+      this.sendAction('update', rental, params);
     },
     delete(rental){
       if(confirm('Do you REALLY want to delete this rental?')){
