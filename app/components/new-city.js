8ec34ec8c5f797cf551ca6b928084be714b8df98
jared beckler and yvonna contreras <yvonnap.contreras@gmail.com>
2016-03-30 09:25:19 -0700

    Add cityFormShow function to new-city.js

---
+++ app/components/new-city.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewCity: false,
+  actions: {
+    cityFormShow(){
+      this.set("addNewCity", true)
+    },
+
+    save1(){
+    var params = {
+      name: this.get('name'),
+      country: this.get('country')
+    };
+    this.set('addNewCity', false);
+    this.sendAction("save2", params);
+  }
+  }
+});
