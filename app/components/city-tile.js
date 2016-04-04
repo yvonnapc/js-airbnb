cfc52a24c84b67d4d33f7bb26a8d55856e350123
jon sweeney and yvonna contreras <jonsweeney26@gmail.com>
2016-04-04 09:31:15 -0700

    Insert fullLocation into city-tile template

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') + ', ' + this.get('city.country');
+  }),
+
 });
